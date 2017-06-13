318afe356904de190bbdcfe567aee81aab060fe8
Arjun Gupta <saimzn4241@gmail.com>
2017-06-13 21:23:30 +0530

    init

---
+++ movies.js
@@ -0,0 +1,14 @@
+
+
+// module.exports.avatar = printAvatar;
+
+module.exports = {
+  printAvatar: function(){
+    console.log("Avatar: PG13");
+  },
+  printChappie: function(){
+    console.log("Chappie: R");
+  },
+  favMovie: "akjdf",
+
+}
